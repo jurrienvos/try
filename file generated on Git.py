10ea76c04f5291dee7c0b0bc6b6aa527d1fb36b9
Jurrien Vos <Jurrien.Vos@ses.com>
2024-09-11 16:06:34 +0200

    3rd sentence

--- file generated on Git.py
+++ file generated on Git.py
@@ -1,2 +1,3 @@
 print("I generate a file on Git to be pulled into main branch on Pycharm")
 print("I generate a file on Git to be pulled into main branch on Pycharm and I made a change")
+print("3rd sentence")

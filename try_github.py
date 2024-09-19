798864ee6fd22e1904c63e68b2dbb6f05d28c846
jurrienvos <57714621+jurrienvos@users.noreply.github.com>
2024-09-19 16:57:33 +0200

    but first figure out,
    #but first figure out, after fetching, how you can see all commits that have been made in the remote repo without knowing which branches are involved and what the changes are.

--- try_github.py
+++ try_github.py
@@ -1,4 +1,5 @@
 
 #the branch called status is about ot be deleted in local repo! 9-19
+#but first figure out, after fetching, how you can see all commits that have been made in the remote repo without knowing which branches are involved and what the changes are.
 
 
